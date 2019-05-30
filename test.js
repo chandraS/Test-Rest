2ef10ae94657979511d6afbd9d8e8a8a744bba11
chandraS <chandra.shikhar@gmail.com>
2019-05-30 14:56:38 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello !'));
 app.get('/person', (req, res, next) => res.json(["Tony","Lisa","Michael"]));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
